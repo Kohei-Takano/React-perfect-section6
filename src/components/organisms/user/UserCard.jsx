e107952471bb7f46ed13bd4c476ff777fb1f80fb
Kohei Takano <132127471+Kohei-Takano@users.noreply.github.com>
2024-07-03 14:05:17 +0000

    32の画像を表示するところまで

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,21 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1518717758536-85ae29035b6d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8fDB8fHww"
+        alt="プロフィール"
+      />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>11111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>090-9999-1111</dd>
+        <dt>会社名</dt>
+        <dd>ああいしす会社</dd>
+        <dt>Web</dt>
+        <dd>https://kokekokko.co.jp</dd>
+      </dl>
+    </div>
+  );
+};
