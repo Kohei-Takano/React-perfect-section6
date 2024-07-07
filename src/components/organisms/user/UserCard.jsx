6fe14672a64fb9030110d784f5b4486ce883f073
Kohei Takano <132127471+Kohei-Takano@users.noreply.github.com>
2024-07-07 15:39:58 +0000

    セクション6終了

--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,21 +1,35 @@
-export const UserCard = () => {
+import styled from "styled-components";
+import { Card } from "../../atoms/card/Card";
+import { UserIconWithName } from "../../molecules/user/UserIconWithName";
+
+export const UserCard = (props) => {
+  const { user } = props;
   return (
-    <div>
-      <img
-        src="https://images.unsplash.com/photo-1518717758536-85ae29035b6d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8fDB8fHww"
-        alt="プロフィール"
-      />
-      <p>名前</p>
-      <dl>
+    <Card>
+      <UserIconWithName image={user.image} name={user.name} />
+      <SDl>
         <dt>メール</dt>
-        <dd>11111@aaa.com</dd>
+        <dd>{user.email}</dd>
         <dt>TEL</dt>
-        <dd>090-9999-1111</dd>
+        <dd>{user.phone}</dd>
         <dt>会社名</dt>
-        <dd>ああいしす会社</dd>
+        <dd>{user.company.name}</dd>
         <dt>Web</dt>
-        <dd>https://kokekokko.co.jp</dd>
-      </dl>
-    </div>
+        <dd>{user.website}</dd>
+      </SDl>
+    </Card>
   );
 };
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dl {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+    overflow-wrap: break-word;
+  }
+`;
